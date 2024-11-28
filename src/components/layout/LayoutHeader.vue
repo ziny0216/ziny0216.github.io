83b356a60b8ee5a8d05db4823f2253d42beddbd1
joyeojin <rkfwoa02@naver.com>
2024-11-28 16:50:12 +0900

    모버알 헤더 수정

--- src/components/layout/LayoutHeader.vue
+++ src/components/layout/LayoutHeader.vue
@@ -1,11 +1,16 @@
 <script setup>
-  import { onBeforeUnmount, onMounted, reactive } from 'vue';
+  import { onBeforeUnmount, onMounted, reactive, ref } from 'vue';
   import { useRouter } from 'vue-router';
 
   const router = useRouter();
+
+  const layoutHeaderRef = ref(null);
   const state = reactive({
     activeTab: 'about',
+    initialOffsetTop: null,
   });
+
+  // 네비 클릭시 active
   const handleSection = sectionId => {
     const element = document.getElementById(sectionId);
     state.activeTab = sectionId;
@@ -14,10 +19,31 @@
     }
   };
 
+  // 모바일 네비 transform
+  const stickyNavi = () => {
+    if (window.innerWidth < 550) {
+      if (!state.initialOffsetTop) {
+        state.initialOffsetTop = layoutHeaderRef.value.offsetHeight;
+      }
+
+      if (layoutHeaderRef.value) {
+        layoutHeaderRef.value.style.transform =
+          window.scrollY >= state.initialOffsetTop
+            ? `translateY(-52px) translateX(-50%)`
+            : `translateX(-50%)`;
+      }
+    } else {
+      if (layoutHeaderRef.value) {
+        layoutHeaderRef.value.style.transform = `translateX(-50%)`;
+      }
+    }
+  };
+
   onMounted(async () => {
+    //observer 섹션 감지 및 activeTab 값 저장
     await router.isReady();
     const options = {
-      root: null, // 기본적으로 뷰포트를 기준으로
+      root: null,
       rootMargin: '0px',
       threshold: 0.5,
     };
@@ -33,16 +59,20 @@
     const sections = document.querySelectorAll('[class*="_section"]');
 
     sections.forEach(section => observer.observe(section));
+
+    window.addEventListener('scroll', stickyNavi);
   });
 
-  onBeforeUnmount(() => {});
+  onBeforeUnmount(() => {
+    window.removeEventListener('scroll', stickyNavi);
+  });
 </script>
 
 <template>
-  <header class="layout_header">
+  <header ref="layoutHeaderRef" class="layout_header">
     <nav class="header_inner inner">
       <h1 class="title">YOEJIN</h1>
-      <ul class="header_navi">
+      <ul class="header_navi sticky">
         <li
           :class="[{ active: state.activeTab === 'about' }, 'navi_item']"
           @click="handleSection('about')"
