ec2d732f4ff5a08e4623d3e72817f8551df96d5d
joyeojin <rkfwoa02@naver.com>
2024-11-28 17:22:32 +0900

    새로고침 404 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from 'vue-router';
 
 const routes = [
   {
@@ -23,7 +27,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
