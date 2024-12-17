6447fc601c0d3b9e94635035debb7373479a2b57
joyeojin <rkfwoa02@naver.com>
2024-12-17 20:07:14 +0900

    observer 버그 수정

--- src/components/layout/LayoutHeader.vue
+++ src/components/layout/LayoutHeader.vue
@@ -45,7 +45,7 @@
     const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.5,
+      threshold: 0.2,
     };
 
     const observer = new IntersectionObserver(entries => {
